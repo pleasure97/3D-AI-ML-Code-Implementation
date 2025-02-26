5dca3f5007dc3b9932dab4a3d634606cbc4cfaf0
conple21 <conple21@gmail.com>
2025-02-26 11:24:06 +0900

    0226 (2) DiffusionGS - Implement Preprocessing and Reference-Point Plucker Coordinates

---
+++ 2025/DiffusionGS/RPPC.py
@@ -0,0 +1,61 @@
+# Source Code : https://github.com/echen01/ray-conditioning/blob/8e1d5ae76d4747c771d770d1f042af77af4b9b5d/training/plucker.py
+import torch
+from torch.nn import functional as F
+
+def get_rays(H, W, intrinsics, c2w, jitter=False):
+  """
+  H : image height
+  W : image width
+  intrinsics : 4 by 4 intrinsic matrix
+  c2w : 4 by 4 camera to world extrinsic matrix
+  """
+  u, v = torch.meshgrid(torch.arange(W, device=c2w.device), torch.arange(H, device=c2w.device), indexing="ij")
+  B = c2w.shape[0]
+  u, v = u.reshape(-1), v.reshape(-1)
+  u_noise = v_noise = 0.5
+  if jitter:
+    u_noise = torch.rand(u.shape, device=c2w.device)
+    v_noise = torch.rand(v.shape, device=c2w.device)
+  u, v = u + u_noise, v + v_noise # add half pixel
+  pixels = torch.stack((u, v, torch.ones_like(u)), dim=0) # (3, H * W)
+  pixels = pixels.unsqueeze(0).repeat(B, 1, 1) # (B, 3 , H * W)
+  if intrinsics.sum() == 0:
+    inv_intrinsics = torch.eye(3, device=c2w.device).tile(B, 1, 1)
+  else:
+    inv_intrinsics = torch.linalg.inv(intrinsics)
+  rays_d = inv_intrinsics @ pixels # (B, 3, H * W)
+  rays_d = c2w[:, :3, :3] @ rays_d
+  rays_d = rays_d.transpose(-1, -2) # (B, H * W, 3)
+  rays_d = F.normalize(rays_d, dim=-1)
+
+  rays_o = c2w[:, :3, 3].reshape((-1, 3)) # (B, 3)
+  rays_o = rays_o.unsqueeze(1).repeat(1, H * W, 1) # (B, H * W, 3)
+
+  return rays_o, rays_d
+
+def plucker_embedding(H, W, intrinsics, c2w, jitter=False):
+  """Computes the plucker coordinates from batched cam2world & intrinsics matrices, as well as pixel coordinates
+  C2W : (Batch Size, 4, 4)
+  intrinsics : (Batch Size, 3, 3)
+  """
+  rays_o, rays_d = get_rays(H, W, intrinsics, c2w, jitter=jitter) # (B, H * W, 3), (B, H * W, 3)
+  cross = torch.cross(rays_o, rays_d, dim=-1)
+  plucker = torch.cat((rays_d, cross), dim=1)
+
+  plucker = plucker.view(-1, H, W, 6).permute(0, 3, 1, 2)
+  return plucker # (B, 6, H, W, )
+
+def reference_point_plucker_embedding(H, W, intrinsics, c2w, jitter=False):
+  """Computes the reference point plucker coordinates from batched cam2world & intrinsics matrices, as well as pixel coordinates
+  H : image height
+  W : image width
+  C2W : (Batch Size, 4, 4)
+  intrinsics : (Batch Size, 3, 3)
+  """
+  rays_o, rays_d = get_rays(H, W, intrinsics, c2w, jitter=jitter) # (B, H * W, 3), (B, H * W, 3)
+  o_dot_d = (rays_o * rays_d).sum(dim=-1, keepdim=True) # (B, H * W , 1)
+  reference_point = rays_o - o_dot_d * rays_d # (B, H * W, 3)
+  reference_point_plucker = torch.cat((rays_d, reference_point), dim=1)
+
+  reference_point_plucker = reference_point_plucker.view(-1, H, W, 6).permute(0, 3, 1, 2)
+  return reference_point_plucker # (B, 6, H, W)
---
+++ 2025/DiffusionGS/preprocess.py
@@ -0,0 +1,22 @@
+import trimesh
+import numpy as np
+
+def center_scale_mesh(mesh: trimesh.Trimesh):
+
+  # Calculate the minimum, maximum bound and center
+  min_bound = mesh.vertices.min(axis=0)
+  max_bound = mesh.vertices.max(axis=0)
+  center = (min_bound + max_bound) / 2
+  print("Mininum bound of Original: ", min_bound)
+  print("Maximum bound of Original: ", max_bound)
+  print("Center: ", center)
+
+  # Move all the vertices to center
+  mesh.vertices -= center
+
+  bound_size = max_bound - min_bound
+  max_extent = np.max(bound_size)
+
+  mesh.vertices /= (max_extent / 2)
+
+  return mesh
