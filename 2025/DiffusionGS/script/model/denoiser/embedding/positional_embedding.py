66812fe4141fe3276325f8d8e0bc5946fd1deb8f
conple21 <conple21@gmail.com>
2025-04-01 10:57:20 +0900

    0401 (1) DiffusionGS - Update Model

--- 2025/DiffusionGS/script/model/denoiser/embedding/positional_embedding.py
+++ 2025/DiffusionGS/script/model/denoiser/embedding/positional_embedding.py
@@ -1,41 +1,13 @@
-from dataclasses import dataclass
-from jaxtyping import Float
-from torch import nn, Tensor
-
-@dataclass
-class PatchConfig:
-    image_size: Float[Tensor]
-    patch_size: int
+import torch
+from torch import nn
 
 
-
-class PatchEmbedding(nn.Module):
-    """Turns a 2D input image into a 1D sequence learnable embedding vector.
-
-    Args:
-        in_channels (int): Number of color channels for the input images. Defaults to 3.
-        patch_size (int): Size of patches to convert input image into. Defaults to 16.
-        embedding_dim (int): Size of embedding to turn image into. Defaults to 768.
-    """
-    def __init__(self,
-                 in_channels:int=9,
-                 patch_size:int=16,
-                 embedding_dim:int=768):
+class PositionalEmbedding(nn.Module):
+    def __init__(self, num_patches: int, embedding_dim: int, device: torch.device):
         super().__init__()
+        self.positional_embedding = nn.Parameter(
+            torch.ones(1, num_patches, embedding_dim), requires_grad=True
+        ).to(device)
 
-        self.in_channels = in_channels
-        self.patch_size = patch_size
-        self.embedding_dim = embedding_dim
-
-        self.patcher = nn.Conv2d(in_channels=in_channels,
-                                 out_channels=embedding_dim,
-                                 kernel_size=patch_size,
-                                 stride=patch_size,
-                                 padding=0)
-
-        self.flatten = nn.Flatten(start_dim=2, end_dim=-1)
-
-    def forward(self, x):
-        x_patched = self.patcher(x)
-        x_flattened = self.flatten(x_patched)
-        return x_flattened.permute(0, 2, 1)
+    def forward(self):
+        return self.positional_embedding
--- 2025/DiffusionGS/script/model/model.py
+++ 2025/DiffusionGS/script/model/model.py
@@ -4,40 +4,90 @@ from lightning.pytorch.loggers.wandb import WandbLogger
 from typing import Optional
 from pathlib import Path
 from torch import nn
+from denoiser.embedding.timestep_embedding import TimestepMLP
+from denoiser.embedding.positional_embedding import PatchEmbedding
 from denoiser.backbone.transformer_backbone import TransformerBackbone
 from decoder.decoder import GaussianDecoder
-from denoiser.embedding.positional_embedding import Path
+from ..loss.loss import Loss
+from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingLR, LambdaLR, SequentialLR
 
 
 @dataclass
 class OptimizerConfig:
     learning_rate: float
+    total_steps: int
     warmup_steps: int
+
 
 @dataclass
 class TrainConfig:
     pass
+
 
 @dataclass
 class TestConfig:
     output_path: Path
 
 
-
 class DiffusionGS(LightningModule):
     logger: Optional[WandbLogger]
-    timestep_mlp: nn.Module
-    patchify_mlp: nn.Module
+    timestep_mlp: TimestepEmbedding,
+    patchify_mlp: PatchifyEmbedding,
     transformer_backbone: TransformerBackbone
     gaussian_decoder: GaussianDecoder
     losses: nn.ModuleList
     optimizer_config: OptimizerConfig
     train_config: TrainConfig
     test_config: TestConfig
-    step_tracker: None
+    step_tracker: None  # TODO - Whether to use step tracker
 
     def __init__(self,
-                 timestep_mlp: TimestepMLP,
-                 patchify_mlp: PatchifyMLP,
-                 ):
+                 optimizer_config: OptimizerConfig,
+                 train_config: TrainConfig,
+                 test_config: TestConfig,
+                 timestep_mlp: TimestepEmbedding,
+                 patchify_mlp: PatchifyEmbedding,
+                 transformer_backbone: TransformerBackbone,
+                 gaussian_decoder: GaussianDecoder,
+                 losses: list[Loss],
+                 step_tracker: None) -> None:
+        super().__init__()
+        self.optimizer_config = optimizer_config
+        self.train_config = train_config
+        self.test_config = test_config
+        self.step_tracker = step_tracker
 
+    def training_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        # TODO - Run the model
+        TimestepMLP(...)
+
+        # TODO - Compute the metrics
+
+        # TODO - Compute the loss
+
+        return total_loss
+
+    @rank_zero_only
+    def validation_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        pass
+
+    def test_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        pass
+
+    def configure_optimizers(self):
+        optimizer = Adam(self.parameters(), lr=self.optimizer_config.learning_rate)
+        warmup_scheduler = LambdaLR(optimizer,
+                                    lr_lambda=lambda epoch, warmup_iters: epoch / warmup_iters if epoch < warmup_iters else 1)
+        cosine_annealing_scheduler = CosineAnnealingLR(optimizer,
+                                                       T_max=self.optimizer_config.total_steps - self.optimizer_config.warmup_steps, eta_min=0)
+        scheduler = SequentialLR(optimizer,
+                                 schedulers=[warmup_scheduler, cosine_annealing_scheduler],
+                                 milestones=[self.optimizer_config.warmup_steps])
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "interval": "step",
+                "frequency": 1}
+        }
