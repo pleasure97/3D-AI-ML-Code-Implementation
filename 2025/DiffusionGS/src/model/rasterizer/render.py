c4f0591a10935446ad0b516d96660978c0e1f465
conple21 <conple21@gmail.com>
2025-04-15 11:08:06 +0900

    0415 (2) DiffusionGS - Update Differentiable Rasterizer

--- 2025/DiffusionGS/src/model/rasterizer/render.py
+++ 2025/DiffusionGS/src/model/rasterizer/render.py
@@ -1,8 +1,8 @@
+from dataclasses import dataclass
 from fast_gauss import GaussianRasterizationSettings, GaussianRasterizer
 from jaxtyping import Float
 from torch import Tensor
 from src.utils.geometry_util import get_fov
-
 
 def render(
         extrinsics: Float[Tensor, "batch 4 4"],
@@ -24,27 +24,45 @@ def render(
     tan_fov_x = (0.5 * fov_x).tan()
     tan_fov_y = (0.5 * fov_y).tan()
 
+    # TODO - make_projection_matrix
+    projection_matrix = make_projection_matrix(intrinsics, near, far, image_shape)
+
+    camera_pose = extrinsics[:, :3, 3]
+
+    # TODO - sh_degree
+    sh_degree = gaussian_covariances.shape[-1]
+
+    # TODO - scale_modifier, prefiltered, debug (Maybe Use Config dataclass)
+
+    # TODO - make means2D
+    means2D = project_means(gaussian_means, extrinsics, projection_matrix, image_shape)
+
+    # TODO - shs, spherical harmonics
 
     settings = GaussianRasterizationSettings(
-        image_height=,
-        image_width=,
-        tanfovx=,
-        tanfovy=,
-        bg=,
-        scale_modifier=,
-        viewmatrix=,
-        projmatrix=,
-        sh_degree=,
-        campos=,
-        prefiltered=,
-        debug=
+        image_height=height,
+        image_width=width,
+        tanfovx=tan_fov_x,
+        tanfovy=tan_fov_y,
+        bg=background_color,
+        scale_modifier=1.,
+        viewmatrix=extrinsics,
+        projmatrix=projection_matrix,
+        sh_degree=sh_degree,
+        campos=camera_pose,
+        prefiltered=False,
+        debug=False
     )
+
+
 
     rasterizer = GaussianRasterizer(settings)
     image, radii = rasterizer(
-        means3D=,
-        means2D=,
+        means3D=gaussian_means,
+        means2D=means2D,
         shs=None,
-        colors_precomp=,
-        opacities=,
-        cov3D_precomp=,)
+        colors_precomp=colors,
+        opacities=gaussian_opacities,
+        cov3D_precomp=gaussian_covariances,)
+
+    return image
