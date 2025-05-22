be089f862b8c8c2f00937f1e1b3d666f2df2a272
conple21 <conple21@gmail.com>
2025-05-22 10:58:46 +0900

    0522 (1) DiffusionGS - Change Multiprocessing from SpinLock to RLock

--- 2025/DiffusionGS/src/utils/step_tracker.py
+++ 2025/DiffusionGS/src/utils/step_tracker.py
@@ -1,14 +1,17 @@
-from src.utils.spin_lock import SpinLock
+# from src.utils.spin_lock import SpinLock
+from torch.multiprocessing import Manager
+from multiprocessing import RLock
 import torch
 from jaxtyping import Int64
 from torch import Tensor
 
 class StepTracker:
-    lock: SpinLock
+    lock: RLock
     step: Int64[Tensor, ""]
 
     def __init__(self):
-        self.lock = SpinLock()
+        # self.lock = SpinLock()
+        self.lock = Manager().RLock()
         self.step = torch.tensor(0, dtype=torch.int64).share_memory_()
 
     def set_step(self, step: int) -> None:
