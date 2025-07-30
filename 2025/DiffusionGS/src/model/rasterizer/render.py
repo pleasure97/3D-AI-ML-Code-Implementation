31188c004d8fb40db54d1f3b17319661c2a188d1
conple21 <conple21@gmail.com>
2025-07-30 10:50:56 +0900

    0730 (1) DiffusionGS - Change Gaussian Rasterizer from fast-gauss to gsplat

--- 2025/DiffusionGS/src/model/rasterizer/render.py
+++ 2025/DiffusionGS/src/model/rasterizer/render.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass
-from fast_gauss import GaussianRasterizationSettings, GaussianRasterizer
+import gsplat
 from jaxtyping import Float
 from torch import Tensor
 import torch
 from src.model import ModuleWithConfig
-from src.utils.geometry_util import get_fov, make_projection_matrix
 
 @dataclass
 class RenderConfig:
@@ -13,75 +12,67 @@ class RenderConfig:
     prefiltered: bool
     debug: bool
 
+
 class GaussianRenderer(ModuleWithConfig[RenderConfig]):
     def __init__(self, config: RenderConfig):
         super().__init__(config)
         self.config = config
-        self.rasterizer = None
 
     def render(
             self,
             extrinsics: Float[Tensor, "batch 4 4"],
             intrinsics: Float[Tensor, "batch 3 3"],
-            near: Float[Tensor, "batch"],
-            far: Float[Tensor, "batch"],
             image_shape: tuple[int, int],
-            background_color: Float[Tensor, "batch 3"],
             colors: Float[Tensor, "batch 3"],
             gaussian_means: Float[Tensor, "batch gaussian 3"],
             gaussian_covariances: Float[Tensor, "batch gaussian 3 sh_degree"],
             gaussian_opacities: Float[Tensor, "batch gaussian"],
+            background_white: True
     ) -> Float[Tensor, "batch 3 height width"]:
 
-        batch_size = extrinsics.shape[0]
-        gaussians = gaussian_means.shape[1]
+        batch_size, views = extrinsics.shape[:2]
         height, width = image_shape[-2], image_shape[-1]
+        num_gaussians = gaussian_means.shape[1]
+        device = gaussian_means.device
 
-        # FOV & Projection
-        fov_x, fov_y = get_fov(intrinsics).unbind(dim=-1)
-        tan_fov_x = (0.5 * fov_x).tan()
-        tan_fov_y = (0.5 * fov_y).tan()
+        if background_white:
+            background_color = torch.zeros((batch_size, 3), device=device)
+        else:
+            background_color = torch.ones((batch_size, 3), device=device)
 
-        projection_matrix = make_projection_matrix(near, far, tan_fov_x, tan_fov_y)
+        images = torch.zeros(batch_size, views, 3, height, width, device=device, dtype=torch.float32)
 
-        # Compute 2D means from 3D Gaussian Centers
-        ones = torch.ones(batch_size, gaussians, 1, device=gaussian_means.device)
-        homogeneous_points = torch.cat([gaussian_means, ones], dim=-1)
-        camera_points = homogeneous_points @ extrinsics.transpose(-2, -1)
+        for batch in range(batch_size):
+            for view in range(views):
+                means = gaussian_means[batch]
+                covariances = gaussian_covariances[batch]
+                opacities = gaussian_opacities[batch]
+                batch_colors = colors[batch]
 
-        # Camera to Clip
-        clip = (projection_matrix @ camera_points.unsqueeze(-1)).squeeze(-1)
-        normalized_coordinates = clip[..., :3] / clip[..., 3:].clamp(min=1e-6)
-        pixel_x = (normalized_coordinates[..., 0] * 0.5 + 0.5) * width
-        pixel_y = (normalized_coordinates[..., 1] * 0.5 + 0.5) * height
-        means2D = torch.stack([pixel_x, pixel_y], dim=-1)
+                batch_extrinsics = extrinsics[batch, view : view + 1]
+                batch_intrinsics = intrinsics[batch, view : view + 1]
+                background = background_color[batch : batch + 1]
 
-        background = background_color.view(1, 3, 1, 1)
-        background = background.expand(batch_size, 3, height, width)
+                quaternions = torch.zeros(num_gaussians, 4, device=device)
+                scales = torch.zeros(num_gaussians, 3, device=device)
 
-        settings = GaussianRasterizationSettings(
-            image_height=height,
-            image_width=width,
-            tanfovx=tan_fov_x,
-            tanfovy=tan_fov_y,
-            bg=background,
-            scale_modifier=self.config.scale_modifier,
-            viewmatrix=extrinsics,
-            projmatrix=projection_matrix,
-            sh_degree=self.config.sh_degree,
-            campos=extrinsics[:, :3, 3],
-            prefiltered=self.config.prefiltered,
-            debug=self.config.debug
-        )
+                output_colors, output_alphas, meta = gsplat.rasterization(
+                    means=means,
+                    quats=quaternions,
+                    scales=scales,
+                    opacities=opacities,
+                    colors=batch_colors,
+                    viewmats=batch_extrinsics,
+                    Ks=batch_intrinsics,
+                    width=width,
+                    height=height,
+                    covars=covariances,
+                    backgrounds=background,
+                    sh_degree=None,
+                    packed=False,
+                    render_mode='RGB')
 
-        if self.rasterizer is None:
-            self.rasterizer = GaussianRasterizer(settings)
-        image, radii = self.rasterizer(
-            means3D=gaussian_means,
-            means2D=means2D,
-            shs=None,
-            colors_precomp=colors,
-            opacities=gaussian_opacities,
-            cov3D_precomp=gaussian_covariances)
+                image = output_colors[0].permute(2, 0, 1)  # [3, height, width]
+                images[batch, view] = image
 
-        return image
+        return images
